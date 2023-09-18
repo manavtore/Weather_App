1c3f2cba74ab1b54970d3fe32dd76c3f21ab4023
manavtore <toremanav@gmail.com>
2023-09-19 00:55:21 +0530

    Initial Commit

---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Manav Tore on 19/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
