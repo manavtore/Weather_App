d577db105893d20a515f52d519457277cfd225af
manavtore <toremanav@gmail.com>
2023-10-01 02:20:43 +0530

    just some update

--- Weather App/ContentView.swift
+++ Weather App/ContentView.swift
@@ -6,78 +6,66 @@
 //
 
 import SwiftUI
+
 struct ContentView: View {
     @State private var weatherData: WeatherData?
+    
     var body: some View {
         ZStack() {
             LinearGradient(colors: [.blue, Color(.lightblue) ], startPoint: .topLeading, endPoint: .bottomTrailing)
                 .edgesIgnoringSafeArea(.all)
             
-            VStack (spacing:8){
+            VStack (spacing: 8) {
                 Text("Delhi, UP")
                     .font(.system(size: 32, weight: .medium, design: .default))
                     .foregroundColor(.white)
                     .padding()
-               
-              
-                VStack(spacing:8){
+                
+                VStack(spacing: 8) {
                     Image(systemName: "cloud.sun.fill")
                         .renderingMode(.original)
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                        .frame(width:200,height: 200)
-                 
-                    Text("76°")
-                        .font(.system(size: 84))
-                            .foregroundColor(.white)
-                        
+                        .frame(width: 200, height: 200)
                     
-                  
+                    Text("\(weatherData?.data.Timelines[0].intervals[0].value.temperature ?? 0)°")
+                        .font(.system(size: 84))
+                        .foregroundColor(.white)
                 }
-//                HStack{
-//                    WeatherOfEachDay(
-//                    dayoftheweeek: "Tue", weathercondition: "cloud.sun.rain.fill", Temparature: 74
-//                    )
-//                    WeatherOfEachDay(
-//                    dayoftheweeek: "Wed", weathercondition: "sun.max.fill", Temparature: 76
-//                    )
-//                    WeatherOfEachDay(
-//                    dayoftheweeek: "Thu", weathercondition: "cloud.moon.rain.fill", Temparature: 79
-//                    )
-//                    WeatherOfEachDay(
-//                    dayoftheweeek: "Fri", weathercondition: "cloud.rain.fill", Temparature: 76
-//                    )
-//                    WeatherOfEachDay(
-//                    dayoftheweeek: "Sat", weathercondition: "wind", Temparature: 76
-//                    )
-//                    
-//                }.padding(.bottom,40)
-//                Button{
-//                    print("click me to enter")
-//                }label: {
-//                    Text("Change Day Time")
-//                }.frame(width:200,height: 50)
-//                    .background(Color.white)
-//                    .font(.system(size: 20,weight: .medium,design: .default))
-//                    .cornerRadius(10)
-             
-                Text("\(Int(weatherData.data.Timelines[0].intervals.value.temperature))°")
-                    .font(.system(size:84))
+                
+                if let weatherData = weatherData {
+                    HStack {
+                        ForEach(weatherData.data.Timelines[0].intervals.prefix(5), id: \.StartTime) { interval in
+                            WeatherOfEachDay(
+                                dayoftheweeek: "Day",
+                                weathercondition: "Sun.max.fill",
+                                Temparature: Int(interval.value.temperature)
+                            )
+                        }
+                    }.padding(.bottom, 40)
+                }
+                
+                Text("\(Int(weatherData?.data.Timelines[0].intervals[0].value.temperature ?? 0))°")
+                    .font(.system(size: 84))
                     .foregroundColor(.white)
                 
-                HStack{
-                    ForEach(weatherData?.data.Timelines[0].intervals.prefix(5).id,:\.self.StartTime){ interval in
-                        WeatherOfEachDay(dayoftheweeek: "Day", weathercondition: "Sun.max.fill", Temparature: Int(interval.value.temperature)
-                        )
-                    }
-                }
                 Spacer()
-        
             }
-            
+        }
+        .onAppear {
+            WeatherServices.fetchWeatherData(latitude: 28.6139, longitude: 77.2090) { result in
+                switch result {
+                case .success(let weatherData):
+                    self.weatherData = weatherData
+                case .failure(let error):
+                    print("Error fetching weather data: \(error)")
+                   
+                }
+            }
         }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
